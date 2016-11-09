7d4ba8a794b2ed9c6cc533f61ead5f4c6b057132
aleksey-ka <aleksey_ka@abbyy.com>
2016-11-09 16:13:50 +0300

    Updated copyrights

--- sample-textcapture/app/src/main/java/com/abbyy/mobile/sample/MainActivity.java
+++ sample-textcapture/app/src/main/java/com/abbyy/mobile/sample/MainActivity.java
@@ -1,4 +1,5 @@
-// Copyright (c) ABBYY Production LLC, 2016. All rights reserved.
+// ABBYY Real-Time Recognition SDK 1 © 2016 ABBYY Production LLC
+// ABBYY is either a registered trademark or a trademark of ABBYY Software Ltd.
 
 package com.abbyy.mobile.sample;
 
